d5e4b4ff132921d28753abff992632772e4ed5e3
regina14 <reginazhang14@gmail.com>
2017-11-10 14:10:14 -0800

    Build a web application to add and delete stock info

--- data-producer.py
+++ data-producer.py
@@ -17,19 +17,67 @@ from kafka.errors import(
     KafkaTimeoutError
 )
 
+from flask import(
+	Flask,
+	request,
+	jsonify
+)
+
 # log set up
 logging.basicConfig()
 logger= logging.getLogger('data-producer')
-
 logger.setLevel(logging.DEBUG)
 
-producer = None
-topic_name = None
+## build a web appliction
+app = Flask(__name__)
+app.config.from_envvar('ENV_CONFIG_FILE')
+kafka_broker = app.config['CONFIG_KAFKA_ENDPOINT']
+topic_name = app.config['CONFIG_KAFKA_TOPIC']
+symbols = set()
+
+producer = KafkaProducer(bootstrap_servers= kafka_broker)
 
 # schedular set up
 schedular = BackgroundScheduler()
 schedular.add_executor('threadpool')
 schedular.start()
+
+
+
+
+
+@app.route('/<symbol>', methods=['POST'])
+# get route
+def add_stock(symbol):
+	if not symbol:
+		return jsonify({
+			'error: ':'Stock symbol is not valid.'
+			}), 400
+	if symbol in symbols:
+		pass
+	else:
+		symbol = symbol.encode('utf-8')
+		symbols.add(symbol)
+		logging.info('Add stock fetch job for %s' % symbol)
+		schedular.add_job(fetch_price, 'interval', [symbol], seconds = 1, id = symbol)
+
+	return jsonify(result = list(symbols)), 200
+
+@app.route('/<symbol>', methods=['DELETE'])
+def del_stock(symbol):
+	if not symbol:
+		return jsonify({
+			'error: ':'Stock symbol is not valid.'
+			}), 400
+	if symbol not in symbols:
+		pass
+	else: 
+		symbol = symbol.encode('utf-8')
+		symbols.remove(symbol)
+		logging.info('Remove stock fetch job for %s' % symbol)
+		schedular.remove_job(symbol)
+
+	return jsonify(result = list(symbols)), 200
 
 def fetch_price(symbol):
 	#getQuotes(symbol)
@@ -43,15 +91,11 @@ def fetch_price(symbol):
 
 if __name__ == '__main__':
 	# - parse user command line argeument
-	parser = argparse.ArgumentParser()
-	parser.add_argument('kafka_broker', help = 'the location of kafka broker')
-	parser.add_argument('topic_name', help = 'the kafka topic to write to')
-	args = parser.parse_args()
-	kafka_broker = args.kafka_broker
-	topic_name = args.topic_name
-	producer = KafkaProducer(bootstrap_servers= kafka_broker)
-	schedular.add_job(fetch_price, 'interval', ['AAPL'], seconds = 1, id = 'AAPL')
-
-	while True:
-		pass
-
+	#parser = argparse.ArgumentParser()
+	#parser.add_argument('kafka_broker', help = 'the location of kafka broker')
+	#parser.add_argument('topic_name', help = 'the kafka topic to write to')
+	#args = parser.parse_args()
+	#kafka_broker = args.kafka_broker
+	#topic_name = args.topic_name
+	
+	app.run(host = '0.0.0.0', port = app.config['CONFIG_APPLICATION_PORT'])
